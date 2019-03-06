73902f90d375aba7c85e500be3c82d683ccabcdd
Faris Deshmukh <faris@faris-mercari.home>
2019-03-06 16:42:03 +0000

    Fill out home page and /charlotte

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const express = require('express');
 const handlebars = require('express-handlebars');
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT ? process.env.PORT : 8080;
 
+// Define rendering engine + static directory
 app.engine('handlebars', handlebars());
 app.set('view engine', 'handlebars');
+app.use(express.static('static'));
 
 app.get('/', (req, res) => {
-  res.send('Welcome to farisdeshmukh.com 👋');
+  res.render('home');
 });
 
 app.get('/about', (req, res) => {
@@ -19,8 +21,8 @@ app.get('/cv', (req, res) => {
   res.send('So many amazing qualifications!');
 });
 
-app.get('/archive', (req, res) => {
-  res.send('All the incredible blog posts I constantly write.');
+app.get('/charlotte', (req, res) => {
+  res.render('charlotte');
 });
 
 app.listen(port, () => console.log(`Our app is now listening on port ${port}!`));
