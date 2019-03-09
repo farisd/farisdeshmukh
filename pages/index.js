641b8ab48e15cca5b8629cd275fd7a2f06a99ac5
Faris Deshmukh <farisdeshmukh@gmail.com>
2019-03-09 18:49:42 +0000

    Update content

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,7 @@ import Layout from '../components/Layout';
 export default () => (
   <Layout>
     <div className="body">
-      <p>Welcome to Faris Deshmukh&apos;s website</p>
-      <li><a href="./about">About Faris</a></li>
-      <li><a href="./cv">A list of all Faris&apos; incredible achievements</a></li>
-      <li><a href="https://www.linkedin.com/in/faris-deshmukh/">LinkedIn</a></li>
+      <p>Product manager. Ex onefinestay, Mercari. Soon to be Starling.</p>
     </div>
   </Layout>
 );
