f48e80f6d881131168dc71f7dbf625f301227e47
Faris Deshmukh <farisdeshmukh@gmail.com>
2019-03-09 22:51:34 +0000

    Add copy to site

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ export default () => (
   <Layout>
     <div className="body">
       <p>Product manager. Ex onefinestay, Mercari. Soon to be Starling.</p>
+      <p>This website is mostly to teach myself a little React. The code is on my github.</p>
     </div>
   </Layout>
 );
