181c5d868a327beafb5096e2befdef5d9816bdb0
Faris Deshmukh <farisdeshmukh@gmail.com>
2019-03-09 13:55:08 +0000

    add about, cv pages and link to linkedin

---
+++ pages/cv.js
@@ -0,0 +1,9 @@
+import Layout from '../components/Layout';
+
+export default () => (
+  <Layout>
+    <div className="body">
+      <p>This is where my CV will go.</p>
+    </div>
+  </Layout>
+);
--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default () => (
       <p>Welcome to Faris Deshmukh&apos;s website</p>
       <li><a href="./about">About Faris</a></li>
       <li><a href="./cv">A list of all Faris&apos; incredible achievements</a></li>
-      <li>CV</li>
+      <li><a href="https://www.linkedin.com/in/faris-deshmukh/">LinkedIn</a></li>
     </div>
   </Layout>
 );
