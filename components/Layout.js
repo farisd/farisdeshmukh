9f0aaef9b74c1827d1d259f8bf7984bfe9f13416
Faris Deshmukh <farisdeshmukh@gmail.com>
2019-03-09 13:53:20 +0000

    add proptypes

--- components/Layout.js
+++ components/Layout.js
@@ -1,45 +1,58 @@
 import Link from 'next/link';
 import Head from 'next/head';
+import PropTypes from 'prop-types';
 
-const Layout = ({ children }) => (
-  <div>
-    <Head>
-      <title>Faris Deshmukh</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <div className="app">
-      <header>
-        <h1><Link href={{ pathname: '/' }}>Faris Deshmukh</Link></h1>
-      </header>
-      { children }
-      <footer>
-            Footer
-      </footer>
+const Layout = (props) => {
+  console.log('test');
+  const { children } = props;
+  return (
+    <div>
+      <Head>
+        <title>Faris Deshmukh</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <div className="app">
+        <header>
+          <h1>
+            <Link href={{ pathname: '/' }}>
+              <a href={{ pathname: '/' }}>Faris Deshmukh</a>
+            </Link>
+          </h1>
+        </header>
+        { children }
+        <footer>
+          Footer
+        </footer>
+      </div>
+      <style global jsx>
+        {`
+              :root {
+                  --green:  #65C5D9; 
+                  --white: #F4F5F7;
+                  --light-gray: #EAEEEF;
+              }
+              ...
+              body{...}
+          `}
+
+      </style>
+      <style jsx>
+        {`
+              header{ ...}
+              header h1 a{ ...}
+              h2{....}
+              footer { ...}
+
+          `}
+
+      </style>
     </div>
-    <style global jsx>
-      {`
-            :root {
-                --green:  #65C5D9; 
-                --white: #F4F5F7;
-                --light-gray: #EAEEEF;
-            }
-            ...
-            body{...}
-        `}
+  );
+};
 
-    </style>
-    <style jsx>
-      {`
-            header{ ...}
-            header h1 a{ ...}
-            h2{....}
-            footer { ...}
-
-        `}
-
-    </style>
-  </div>
-);
+Layout.propTypes = {
+  children: PropTypes.element.isRequired,
+};
 
 export default Layout;
