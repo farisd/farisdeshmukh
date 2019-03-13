920c3fe536651803263dcd3276116a6889e4d51b
Faris Deshmukh <farisdeshmukh@gmail.com>
2019-03-13 14:15:04 +0000

    delete about page

--- pages/about.jsx
+++
@@ -1,9 +0,0 @@
-import Layout from '../components/Layout';
-
-export default () => (
-  <Layout>
-    <div className="body">
-      <p>Faris is a really great person. He works as a product manager.</p>
-    </div>
-  </Layout>
-);
