41d7453b5101db5221420e23f20dbb5529d06e6d
Faris Deshmukh <farisdeshmukh@gmail.com>
2019-03-14 12:11:07 +0000

    convert standard media elements to media components

--- pages/cv.jsx
+++ pages/cv.jsx
@@ -1,12 +1,13 @@
 import MarkdownContent from '../components/markdownContent';
 import Layout from '../components/Layout';
+import MediaContent from '../components/mediaContent';
 
 const summaryCopy = `
   ## Products I have helped to make
 `;
 
 const mercariCopy = `
-  ### Mercari UK, 2018-19
+  ### Mercari, 2018-19
   Mercari is Japan's largest C2C marketplace, and most successful consumer startup in recent years.
   Mercari closed down in the UK in Q1 2019.
 
@@ -24,28 +25,25 @@ const onefinestayCopy = `
   Website development effectively stopped since i left  🤷‍♂️, so [check it out](https://www.onefinestay.com) in all its 2018 glory!
 `;
 
-
 export default () => (
   <Layout>
     <div className="body">
       <MarkdownContent markup={summaryCopy} />
       <MarkdownContent markup={mercariCopy} />
-      <img className="halfImage" src="https://storage.googleapis.com/farisdeshmukh-com/images/mercari_old.png" height="500" alt="mercari homepage from 2017" />
-      <video className="halfImage" width="300" height="500" muted controls>
-        <source src="https://storage.googleapis.com/farisdeshmukh-com/videos/mercari_new.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
+      <div id="mercariComparison">
+        <MediaContent type="image" size="half" location="https://storage.googleapis.com/farisdeshmukh-com/images/mercari_old.png" alt="mercari homepage from 2017" />
+        <MediaContent type="video" size="half" location="https://storage.googleapis.com/farisdeshmukh-com/videos/mercari_new.mp4" />
+      </div>
       <MarkdownContent markup={onefinestayCopy} />
-      <img className="fullImage"  src="https://storage.googleapis.com/farisdeshmukh-com/images/onefinestay.png" alt="onefinestay homepage" />
+      <MediaContent type="image" size="full" location="https://storage.googleapis.com/farisdeshmukh-com/images/onefinestay.png" alt="onefinestay homepage" />
     </div>
     <style jsx>
       {
       `
-      .fullImage {
-        max-width: 100%;
-        width: 600px;
+      #mercariComparison {
+        display: flex;
       }
-    `
+      `
       }
     </style>
   </Layout>
