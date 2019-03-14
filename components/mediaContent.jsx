5349fc47a63ed3e3b38cd38d5a719d58ff7ce666
Faris Deshmukh <farisdeshmukh@gmail.com>
2019-03-14 12:10:47 +0000

    componentize basic image and video elements

---
+++ components/mediaContent.jsx
@@ -0,0 +1,49 @@
+const styles = (
+  <style jsx>
+    {
+      `
+    .fullImage {
+      max-width: 100%;
+      width: 600px;
+    }
+    .halfImage {
+      max-width: 100%;
+      width: 300px;
+    }
+    .fullVideo {
+      max-width: 100%;
+      width: 600px;
+    }
+    .halfVideo {
+      max-width: 100%;
+      width: 300px;
+    }
+    `
+    }
+  </style>
+);
+
+const MediaContent = (props) => {
+  const {
+    type, size, altText, location,
+  } = props;
+  if (type === 'image') {
+    return (
+      <div>
+        <img className={`${size}Image`} src={location} alt={altText} />
+        {styles}
+      </div>
+    );
+  } if (type === 'video') {
+    return (
+      <video className={`${size}Video`} height="500" muted controls>
+        <source src={location} type="video/mp4" />
+        Your browser does not support the video tag.
+        {styles}
+      </video>
+    );
+  }
+  throw Error;
+};
+
+export default MediaContent;
